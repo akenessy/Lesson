020d045ea0e4bfbe5327e5d184b57ed835bd9b31
akenessy <zraev2004@gmail.com>
2025-10-25 16:46:05 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let fisrtNumber = 1; //задаем первое число
+let secondNumber = 10; //задаем второе число 
+
+if (fisrtNumber > secondNumber) { //проверяем больше ли первое число
+    console.log("Наибольшее число = ", fisrtNumber); //если больше выводим
+} else { //если меньше первое число
+    console.log("Наибольшее число = ", secondNumber); //значит второе больше выводим его
+}
